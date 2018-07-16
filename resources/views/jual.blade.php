c89724388ccdca771b1a2f2b0e38f2b89dcc238f
Cevin Ways Al Cornelis <cevinways34@gmail.com>
2018-07-16 10:33:05 -0700

    fixing bug on penjualan replace with on maintainance

--- resources/views/jual.blade.php
+++ resources/views/jual.blade.php
@@ -34,6 +34,19 @@
             </ol>
         </nav>
         <h2>Produk Reksadana Anda</h2>
+        <div class="row">
+            <div class="col-md-12 m-bot-6 text-center">
+                <div class="card" style="box-shadow: 0 5px 9px 0 #BDBDBD; border-color: white">
+                    <div class="card-body">
+                        <h1 class="f-35 font-bold uppercase">Masih dalam Perbaikan!</h1>
+                        <div class="hr-line blue-color m-bot-2"></div>
+                        <div><span class="fa fa-3x fa-wrench"></span></div>
+                        <p>Mohon Maaf atas tidak kenyamanan anda</p>
+                        <a href="{{route('shop.index')}}" class="btn btn-rounded btn-primary">Lanjut Belanja</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- Sub Header -->
